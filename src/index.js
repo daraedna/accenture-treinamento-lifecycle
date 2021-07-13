c4102fc4d669604dbbd3b86e4e14f1f22cdaf1b7
Daraédna Menezes <daraedna@gmail.com>
2021-07-13 19:31:57 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class App extends React.Component {
+  state = {
+    clicou: "ainda não clicou"
+  }
+  componentDidMount(){
+    console.log('[App] - Apareceu na Tela');
+  }
+  handleClick(){
+    this.setState({
+      clicou: "Clicou"
+    })
+  }
+  render(){
+    console.log("[App] - Renderizou")
+    return (
+      <div>
+        Olá, {this.props.name}, você {this.state.clicou}
+        <button onClick={this.handleClick.bind(this)}>
+          Clique aqui
+        </button>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name={"Daraédna Menezes"}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
